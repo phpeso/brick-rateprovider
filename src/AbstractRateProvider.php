72b63b707a84af1f04253814d54cb769515e1a9d
Anton Smirnov <sandfox+git@sandfox.me>
2026-04-13 14:41:19 +0300

    Prototype support for Money 0.13

--- src/AbstractRateProvider.php
+++ src/AbstractRateProvider.php
@@ -9,10 +9,19 @@ declare(strict_types=1);
 
 namespace Peso\Brick;
 
-use Brick\Money\Exception\CurrencyConversionException;
+use Arokettu\Date\Calendar;
+use Arokettu\Date\Date;
+use Brick\Math\BigDecimal;
+use Brick\Math\BigNumber;
+use Brick\Money\Currency;
+use Brick\Money\Exception\ExchangeRateProviderException;
 use Brick\Money\ExchangeRateProvider;
+use DateTimeInterface;
 use Error;
 use Override;
+use Peso\Core\Exceptions\RuntimeException;
+use Peso\Core\Requests\CurrentExchangeRateRequest;
+use Peso\Core\Requests\HistoricalExchangeRateRequest;
 use Peso\Core\Responses\ErrorResponse;
 use Peso\Core\Responses\ExchangeRateResponse;
 use Peso\Core\Services\PesoServiceInterface;
@@ -24,27 +33,57 @@ abstract readonly class AbstractRateProvider implements ExchangeRateProvider
     ) {
     }
 
-    abstract protected function createRequest(string $sourceCurrencyCode, string $targetCurrencyCode): object;
+    protected function createRequest(
+        string $sourceCurrencyCode,
+        string $targetCurrencyCode,
+        array $dimensions = [],
+    ): object|null {
+        $date = false;
+
+        foreach ($dimensions as $dimension => $value) {
+            if ($dimension !== 'date') {
+                return null;
+            }
+            if ($value === null) {
+                continue; // allow explicitly set null
+            }
+            if ($value instanceof Date) {
+                $date = $value;
+            }
+            if ($value instanceof DateTimeInterface) {
+                $date = Calendar::fromDateTime($value);
+            }
+            if (\is_string($value)) {
+                $date = Calendar::parse($value);
+            }
+        }
+
+        return $date ?
+            new HistoricalExchangeRateRequest($sourceCurrencyCode, $targetCurrencyCode, $date) :
+            new CurrentExchangeRateRequest($sourceCurrencyCode, $targetCurrencyCode);
+    }
 
     /**
      * @inheritDoc
      */
     #[Override]
-    public function getExchangeRate(string $sourceCurrencyCode, string $targetCurrencyCode): string
-    {
-        $request = $this->createRequest($sourceCurrencyCode, $targetCurrencyCode);
-        $response = $this->service->send($request);
+    public function getExchangeRate(
+        Currency $sourceCurrency,
+        Currency $targetCurrency,
+        array $dimensions = [],
+    ): BigNumber|null {
+        $request = $this->createRequest($sourceCurrency->getCurrencyCode(), $targetCurrency->getCurrencyCode());
+        try {
+            $response = $this->service->send($request);
+        } catch (RuntimeException $e) {
+            throw new ExchangeRateProviderException($e->getMessage(), $e);
+        }
 
         if ($response instanceof ExchangeRateResponse) {
-            return $response->rate->value;
+            return BigDecimal::of($response->rate->value);
         }
         if ($response instanceof ErrorResponse) {
-            throw CurrencyConversionException::exchangeRateNotAvailable(
-                $sourceCurrencyCode,
-                $targetCurrencyCode,
-                $response->exception->getMessage(),
-                $response->exception,
-            );
+            return null;
         }
 
         throw new Error(
